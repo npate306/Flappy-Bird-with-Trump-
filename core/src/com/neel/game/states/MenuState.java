26098a3616a4e1cd8931fd6cb00f41b50d4b4377
NEEL S PATEL <npate306@illinois.edu>
2017-07-04 16:38:09 -0500

    First Commit!

---
+++ core/src/com/neel/game/states/MenuState.java
@@ -0,0 +1,38 @@
+package com.neel.game.states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.neel.game.FlappyDemo;
+
+public class MenuState extends State{
+    Texture background;
+    Texture playBtn;
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playbtn.png");
+    }
+
+    @Override
+    public void handleInput() {
+        if(Gdx.input.justTouched()){
+            gsm.set(new PlayState(gsm));
+        }
+    }
+
+    @Override
+    public void update(float dt) {
+        handleInput();
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        sb.draw(playBtn, (Gdx.graphics.getWidth() / 2) - (playBtn.getWidth() / 2), Gdx.graphics.getHeight() / 2);
+        sb.end();
+
+    }
+}
